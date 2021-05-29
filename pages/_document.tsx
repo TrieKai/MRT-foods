b7484ec14a255e99d4f643969e116aa6a72fdd5f
TrieKai <trie55432@gmail.com>
2021-05-29 14:03:06 +0800

    [feat] Update web icon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -31,7 +31,9 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <link rel="shortcut icon" href="/mrt.svg" />
+        </Head>
         <body>
           <Main />
           <NextScript />
