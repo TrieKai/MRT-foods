7090e439f1e9e2bfe1470b0615ff0f28ca32c170
TrieKai <trie55432@gmail.com>
2022-05-08 16:32:26 +0800

    feat(ga): add Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import Script from 'next/script'
 import { useInterpret } from '@xstate/react'
 import { MyContext } from 'contexts/context'
 import { myMachine } from 'contexts/machine'
@@ -10,6 +11,19 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
 
   return (
     <MyContext.Provider value={{ myService }}>
+      <Script
+        src='https://www.googletagmanager.com/gtag/js?id=G-D15KYJER59'
+        strategy='afterInteractive'
+      />
+      <Script id='google-analytics' strategy='afterInteractive'>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-D15KYJER59');
+        `}
+      </Script>
       <Component {...pageProps} />
     </MyContext.Provider>
   )
